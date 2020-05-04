5d1b06560bad596812ede80720305bbdb8407442
Himanshu Teotia <himanshuteotia7@gmail.com>
2020-05-04 14:02:12 +0530

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -8,3 +8,22 @@ function doSomething <T extends add>(obj:T) {
 }
 
 console.log(doSomething({a:6,b:5}))
+
+
+interface properties {
+   firstName : string;
+   lastName : string;
+   fullName: () => string
+}
+
+class MyName implements properties {
+   constructor(public firstName:string, public lastName:string){
+
+   }
+   fullName() {
+      return this.firstName + ' ' + this.lastName;
+   }
+}
+
+let myname = new MyName('Himanshu', 'Teotia');
+console.log(myname.fullName())
